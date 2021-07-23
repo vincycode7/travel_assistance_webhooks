6cb331e45a4631c8bb9a3acb129349bf81eae363
vincycode7 <akinwandegbengavincent@gmail.com>
2021-07-23 23:11:12 +0100

    implemented more than one response popups

--- index.py
+++ index.py
@@ -2,10 +2,13 @@
 
 from flask import Flask, request, jsonify, render_template
 import os
+
+from werkzeug.wrappers import response
 import dialogflow
 import requests
 import json
 import pusher
+import google.protobuf  as pf
 
 app = Flask(__name__)
 
@@ -42,22 +45,25 @@ def detect_intent_texts(project_id, session_id, text, language_code):
             session=session, query_input=query_input)
                 # for i in range(0,len(response.query_result.fulfillment_messages)):
                 # print(response.query_result.fulfillment_messages[i].text)
-        print({"response_id":response.response_id, "query_result.":response.query_result})
-        return {"response_id":response.response_id, "query_result.":response.query_result}
-        # return response.query_result.fulfillment_messages
+        # print({"response_id":response.response_id, "query_result.":response.query_result})
+        # return {"response_id":response.response_id, "query_result.":response.query_result}
+        return json.loads(pf.json_format.MessageToJson(response, including_default_value_fields=False))
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
     message = request.form['message']
     project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-    fulfillment_message = detect_intent_texts(project_id, "unique", message, 'en')
+    response = detect_intent_texts(project_id, "unique", message, 'en')
+    # print(response, type(response))
     # print(dict(fulfillment_message))
     # text_msgs = []
     # for i in range(0,len(fulfillment_message)):
     #     text_msgs.append(fulfillment_message[i].text["text"])
     #     print(fulfillment_message[i].text)
-    response_msg = { "message":  fulfillment_message }
-    return jsonify(response_msg)
+    # response_msg = { "message":  fulfillment_message }
+    # return fulfillment_message
+    return jsonify(response)
+    # return {"message":"Welcome to Travel portal. How can I help you?"}
 
 # run Flask
 if __name__ == "__main__":
--- static/custom.js
+++ static/custom.js
@@ -2,14 +2,33 @@ function submit_message(message) {
     $.post( "/send_message", {message: message}, handle_response);
 
     function handle_response(data) {
-    
-      console.log(data)
-      // append the bot repsonse to the div
-      $('.chat-container').append(`
-            <div class="chat-message col-md-5 offset-md-7 bot-message">
-                ${data.message}
+       data_msg = data.queryResult.fulfillmentMessages
+      // remove the loading indicator
+      $( "#loading" ).remove();
+      for(let i=0; i<data_msg.length; i++){
+        // append the bot repsonse to the div
+        // loading
+        $('.chat-container').append(`
+            <div class="chat-message text-center col-md-2 offset-md-10 bot-message" id="loading">
+                <b>...</b>
             </div>
-      `)
+        `)
+        // console.log("I am setTimeout");
+        // setTimeout(function(){
+        //     alert("I am setTimeout");
+        //     console.log("I am setTimeout");
+
+        // }, 1000); 
+
+        $('.chat-container').append(`
+                <div class="chat-message col-md-5 offset-md-7 bot-message">
+                    ${data_msg[i].text.text[0]}
+                </div>
+        `)
+        $( "#loading" ).remove();
+      }
+    //   console.log(data_msg)
+      
       // remove the loading indicator
       $( "#loading" ).remove();
     }
